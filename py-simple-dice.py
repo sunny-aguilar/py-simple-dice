4eec643dbd94d34a83c74d4e564f808e75ae7d09
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-20 14:18:05 -0500

    dice

--- py-simple-dice.py
+++ py-simple-dice.py
@@ -4,12 +4,12 @@ import random
 # flag to check if user wants to roll again
 go = True
 
-#
+# function that rolls the dice and prints the value
 def roll_dice():
     number = random.randint(1,6)
     print(number)
 
-
+# loop that lets a user roll the dice until they quit
 while go:
     result = roll_dice()
     response = input('Would you like to roll again (y/n)? ')
